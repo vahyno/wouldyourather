8d53faf95bc7aa26ec152ee7679f9adb1e85e772
vahyno <vahyno@gmail.com>
2018-10-07 20:36:49 -0700

    feat: Login.js fetching data to form, style: created login.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import '../styles/login.css';
 import {connect} from 'react-redux';
 
 
@@ -11,14 +12,51 @@ class Login extends Component {
         logged: false,
     }
 
+    onFromSubmit = (e) => {
+        e.preventDefault();
+        console.log('button');
+    }
+
+    handleInputChange = (e) => {
+        const username = e.target.value;
+        this.setState({
+            username
+        });
+    }
+
     render() {
+        const {users} = this.props;
+        console.log('props users:', users);
         return (
-            <div>
-                Login
+            <div className='login'>
+                <form className='login-form right-align row input-field col s6 m2' onSubmit={this.onFromSubmit}>
+                    <h5 className='form-label center-align'>Login to enter:</h5>
+                    <select 
+                        className="waves-effect waves-light blue lighten-4"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        >
+                        <option value='' disabled>Choose existing user</option>
+                        {users.map(user => (
+                            <option key={user.id}>{user.name}</option>
+                        ))}
+                    </select>
+                    <button
+                        className="login-btn waves-effect waves-light red lighten-2 btn" 
+                        type="submit" 
+                        name="action">
+                        Log In
+                    </button>
+                </form>
             </div>
         )
     }
 }
 
+function mapStateToProps({ users }) {
+    return {
+        users: Object.values(users)
+    }
+}
 
-export default (Login);
+export default connect(mapStateToProps)(Login);
