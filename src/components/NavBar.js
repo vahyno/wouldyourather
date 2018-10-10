00b7a9108c267a879bdfcdac3ad4a4aed473436b
vahyno <vahyno@gmail.com>
2018-10-10 00:08:44 -0700

    Feat: NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,17 @@ class NavBar extends Component {
         const {authedUser, users} = this.props;
         console.log('authedUser:', authedUser, 'users:', users);
         return (
-            <div>NavBar</div>
+            <nav>
+                <div className="nav-wrapper blue lighten-2">
+                <a href="#" style={{'marginLeft':'0.5em'}}className="brand-logo">{authedUser}, Would You Rather?</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Leaderboard</a></li>
+                    <li><a href="#">New Question</a></li>
+                    <li><a href="#" style={{'marginRight':'2em'}}>Sign Out</a></li>
+                </ul>
+                </div>
+            </nav>
         )
     }
 }
