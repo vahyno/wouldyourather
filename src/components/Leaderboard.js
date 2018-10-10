fc2fe79e1383abe89abb3587aa2b870607bac842
vahyno <vahyno@gmail.com>
2018-10-10 12:50:54 -0700

    feat: sorting names in Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,11 +2,45 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class Leaderboard extends Component {
+
     render(){
+        const {users} = this.props
+
+        console.log('users to map', users)
         return (
-            <div>Leaderboard</div>
+            <div>
+            <h3>Leaderboard</h3>
+            <ul>
+                {this.props.users.map(userId => (
+                    <li key={userId}>{userId}</li>
+                ))}
+            </ul>
+            </div>
         )
     }
 }
 
-export default connect()(Leaderboard);
+function mapStateToProps({users}) {
+    return {
+        users: Object.keys(users)
+            .sort((a, b) => ((Object.keys(users[b].answers).length + users[b].questions.length) - (Object.keys(users[a].answers).length + users[a].questions.length)))
+    }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
+
+
+// let users = {
+//     sarahedo: {
+//         id: 'sarahedo',
+//         name: 'Sarah Edo',
+//         avatarURL: './avatar.png',
+//         answers: {
+//           "8xf0y6ziyjabvozdd253nd": 'optionOne',
+//           "6ni6ok3ym7mf1p33lnez": 'optionOne',
+//           "am8ehyc8byjqgar0jgpub9": 'optionTwo',
+//           "loxhs1bqm25b708cmbf3g": 'optionTwo'
+//         },
+//         questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+//       },
+  
