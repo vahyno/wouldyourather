131755036eab55b07a246d7870f2042af916e251
vahyno <vahyno@gmail.com>
2018-10-13 08:31:32 -0700

    feat: QuestionInfo component functions fully

--- src/components/QuestionInfo.js
+++ src/components/QuestionInfo.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import UserDetail from './UserDetail';
 import '../styles/questionInfo.css';
 import {handleQuestionAnswer} from '../actions/shared';
@@ -21,6 +22,8 @@ class QuestionInfo extends Component {
             'question:', question,
             'users:', users, 
             'authedUser:', authedUser);
+        const votedOne = question.optionOne.votes.includes(authedUser);
+        const votedTwo = question.optionTwo.votes.includes(authedUser);
 
         return (
             <div>
@@ -32,20 +35,59 @@ class QuestionInfo extends Component {
                                 <UserDetail userId={question.author} />
                             </ul>
 
-                                {/*no answer to question*/}
-                                <ul className='collection'>
-                                    <li className='questionInfo-question-list left btn-large waves-effect waves collection-item'
-                                        onClick={(e) => this.handleVote(e, 'optionOne')}
-                                        >
-                                        {question.optionOne.text}
-                                    </li>
-                                    <li className='questionInfo-question-list btn-large waves-effect waves collection-item'
-                                        onClick={(e) => this.handleVote(e, 'optionTwo')}
-                                        >
-                                        {question.optionTwo.text}
-                                    </li>
-                                </ul>
-                            
+                            {!question.optionOne.votes.includes(authedUser) && !question.optionTwo.votes.includes(authedUser) 
+                                ? ( 
+                                    <ul className='collection with-header'>
+                                        <li className="center collection-header"><h4>Cast Your Vote:</h4></li>
+                                        <li className='questionInfo-question-list left btn-large waves-effect waves collection-item'
+                                            onClick={(e) => this.handleVote(e, 'optionOne')}
+                                            >
+                                            {question.optionOne.text}
+                                        </li>
+                                        <li className='questionInfo-question-list btn-large waves-effect waves collection-item'
+                                            onClick={(e) => this.handleVote(e, 'optionTwo')}
+                                            >
+                                            {question.optionTwo.text}
+                                        </li>
+                                    </ul>
+                                ) 
+                                : (
+                                    <ul className='collection'>
+                                        <li className='left questionInfo-voted-list collection-item'
+                                            style={{'backgroundColor': votedOne? '#e57373':''}}>
+                                            <h6>{question.optionOne.text}</h6>
+                                            <h6>
+                                                {question.optionOne.votes.length 
+                                                ? (`votes: ${question.optionOne.votes.length}`)
+                                                : 'no votes yet'}
+                                            </h6>
+                                            <h6>
+                                                {question.optionOne.votes.length
+                                                ?(`${(question.optionOne.votes.length/(question.optionOne.votes.length+question.optionTwo.votes.length)*100).toFixed(2)}% of all votes`)
+                                                : '0% of all votes'}
+                                            </h6>
+                                        </li>
+                                        <li className='right questionInfo-voted-list collection-item'
+                                            style={{'backgroundColor': votedTwo? '#448aff':''}}>
+                                            <h6>{question.optionTwo.text}</h6>
+                                            <h6>
+                                                {question.optionTwo.votes.length
+                                                ?(`votes: ${question.optionTwo.votes.length}`)
+                                                : 'no votes yet'}
+                                            </h6>
+                                            <h6>
+                                                {question.optionTwo.votes.length
+                                                ?(`${(question.optionTwo.votes.length/(question.optionOne.votes.length+question.optionTwo.votes.length)*100).toFixed(2)}% of all votes`)
+                                                : '0% of all votes'}
+                                            </h6>
+                                        </li>
+                                    </ul>
+                                )
+                            }
+                            <Link to='/' 
+                                className='back-btn waves-effect waves-light blue lighten-2 btn'>
+                                Back
+                            </Link>
                         </div>
                     </div>
                 }
